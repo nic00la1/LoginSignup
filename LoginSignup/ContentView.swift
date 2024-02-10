1102c5b56a872044256d78893f941e03a8101809
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-02-10 14:15:53 +0100

    Initial commit

---
+++ LoginSignup/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  LoginSignup
+//
+//  Created by Nicola Kaleta on 10/02/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!sds")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ LoginSignup/LoginSignupApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginSignupApp.swift
+//  LoginSignup
+//
+//  Created by Nicola Kaleta on 10/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct LoginSignupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
