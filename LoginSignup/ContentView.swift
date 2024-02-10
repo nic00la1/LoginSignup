4c30a8de6886c4d0c3b91869dc7f310090403b69
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-02-10 14:29:09 +0100

    welcomeView design done

--- LoginSignup/ContentView.swift
+++ LoginSignup/ContentView.swift
@@ -16,10 +16,47 @@ struct ContentView: View {
                     .scaledToFit()
                     .frame(width: 370)
                     .padding(.top, 24)
+                Spacer()
+                
                 Text("Discover Your Dream Job Here.")
                     .font(.system(size: 35, weight: .bold))
                     .multilineTextAlignment(.center)
                     .foregroundStyle(.primaryBlue)
+                    .padding(.bottom, 8)
+                
+                Text("Explore all the existing job roles based on your interest and study major")
+                    .font(.system(size: 14, weight: .regular))
+                    .multilineTextAlignment(.center)
+                    .foregroundStyle(.black)
+                
+                Spacer()
+                
+                HStack(spacing: 12) {
+                    
+                    Button {
+                        // action
+                    } label: {
+                        Text("Login")
+                            .font(.system(size: 20, weight: .semibold))
+                            .foregroundStyle(.white)
+                    }
+                    .frame(width: 160, height: 60)
+                    .background(.primaryBlue)
+                    .cornerRadius(12)
+                    
+                    Button {
+                        // action
+                    } label: {
+                        Text("Register")
+                            .font(.system(size: 20, weight: .semibold))
+                            .foregroundStyle(.black)
+                    }
+                    .frame(width: 160, height: 60)
+                    
+                    
+                }
+                
+                Spacer()
             }
             .padding()
         }
