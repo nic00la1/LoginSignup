6c87b0f54b3423b1c17884044eaad919eef425ee
Nicola Kaleta <nic00la.kaleta@gmail.com>
2024-02-10 18:38:43 +0100

    create user is done

--- LoginSignup/ViewModel.swift
+++ LoginSignup/ViewModel.swift
@@ -6,3 +6,34 @@
 //
 
 import Foundation
+
+class ViewModel: ObservableObject {
+    private let service: AppService
+    @Published var showAlert = false
+    @Published var alertMessage = ""
+    @Published var hasError = false
+    @Published var isLoading = false
+    @Published var isLoggedIn = false
+    
+    init(service: AppService) {
+        self.service = service
+    }
+    
+    func createUser(email: String, password: String) async throws {
+        isLoading = true
+        let status = try await service.createUser(email: email, password: password)
+        
+        switch status {
+        case .success:
+            isLoading = false
+            alertMessage = "Your account has been created succesfully!"
+            showAlert.toggle()
+            
+        case .error(let message):
+            isLoading = false
+            hasError = true
+            alertMessage = message
+            showAlert.toggle()
+        }
+    }
+}
